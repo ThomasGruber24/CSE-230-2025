74b371fc23c967f0a4f46b1d19076bc5d7f3d1eb
ThomasGruber24 <thomaspgruber@yahoo.com>
2025-10-10 14:53:10 -0600

    Update acceleration.cpp

--- 230.03.Ponder/acceleration.cpp
+++ 230.03.Ponder/acceleration.cpp
@@ -2,7 +2,7 @@
  * Source File:
  *    ACCELERATION 
  * Author:
- *    Br. Helfrich
+ *    Br. Helfrich Thomas, David
  * Summary:
  *    Everything we need to know about changing speed
  ************************************************************************/
@@ -12,22 +12,61 @@
 
 #include <cmath>
 
-/*********************************************
- * ACCELERATION : ADD
- *  a += a
- *********************************************/
-void Acceleration::add(const Acceleration& acceleration)
+ /*********************************************
+  * ACCELERATION : Default constructor
+  *********************************************/
+Acceleration::Acceleration()
 {
-   ddx = -99.9;
-   ddy = -88.8;
+   ddx = 0.0;
+   ddy = 0.0;
 }
 
 /*********************************************
- * ACCELERATION : SET
- *  set from angle and direction
+ * ACCELERATION : Non-default constructor
  *********************************************/
-void Acceleration::set(const Angle & angle, double magnitude)
+Acceleration::Acceleration(double ddx, double ddy)
 {
-   ddx = -99.9;
-   ddy = -88.8;
+   this->ddx = ddx;
+   this->ddy = ddy;
+}
+
+/*********************************************
+ * ACCELERATION : Getters
+ *********************************************/
+double Acceleration::getDDX() const { return ddx; }
+double Acceleration::getDDY() const { return ddy; }
+
+/*********************************************
+ * ACCELERATION : Setters
+ *********************************************/
+void Acceleration::setDDX(double ddx) { this->ddx = ddx; }
+void Acceleration::setDDY(double ddy) { this->ddy = ddy; }
+
+/*********************************************
+ * ACCELERATION : Set by angle
+ *********************************************/
+void Acceleration::set(const Angle& angle, double magnitude)
+{
+   double radians = angle.getRadians();
+   ddx = magnitude * sin(radians);
+   ddy = magnitude * cos(radians);
+}
+
+/*********************************************
+ * ACCELERATION : Additional functions
+ *********************************************/
+void Acceleration::addDDX(double ddx)
+{
+   this->ddx += ddx;
+}
+
+void Acceleration::addDDY(double ddy)
+{
+   this->ddy += ddy;
+}
+
+void Acceleration::add(const Acceleration& rhs)
+{
+   this->ddx += rhs.ddx;
+   this->ddy += rhs.ddy;
 }
