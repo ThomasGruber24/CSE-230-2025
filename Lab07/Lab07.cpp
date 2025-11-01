2da9bd5befb5d4bee9e14cae4dcb7085b7e8674f
ThomasGruber24 <thomaspgruber@yahoo.com>
2025-10-31 20:10:51 -0600

    Please look at the defines, I defined gravity so it will be easier for us. Had some issues with gravity so I created a second function for it thats more of a helper function.  this helps with cohiesion as well. (pardon spelling) but I have ran the code and it tests correctly

--- Lab07/Lab07.cpp
+++ Lab07/Lab07.cpp
@@ -3,7 +3,9 @@
 
 #include <iostream>
 #include <cmath>
+#include <vector>
 #define PI 3.14159265358979323846264338327950288
+#define GRAVITY -9.8
 
 using namespace std;
 
@@ -38,7 +40,13 @@ double kinematicsEquation(double initialVelocity, double equationAcceleration, d
     return initialVelocity + (equationAcceleration * time);
 }
 
-// ---------- Assignment ----------:
+double linearInterpolation(double r0, double r1, double d0, double d1, double d)
+{
+    return r0 + (r1 - r0) * ((d - d0) / (d1 - d0));
+}
+
+
+// ---------- Assignment ----------
 
 // Inertia function, passed
 void inertia(double initialSpeed, double initialAngle)
@@ -61,12 +69,10 @@ void inertia(double initialSpeed, double initialAngle)
 
 
 // Acceleration function, passed
-void acceleration(double constantGravity)
+void acceleration(double constantGravity, double initialSpeed, double initialAngle)
 {
     double distance = 0.0;
     double altitude = 0.0;
-    double initialSpeed = 827.0;
-    double initialAngle = 75.0;
 
     double angleInRadians = degreesToRadians(initialAngle);
     double dx = horizontalComponent(initialSpeed, angleInRadians);
@@ -107,13 +113,68 @@ void groundImpact(double timeInterval, double gravity, double initialSpeed, doub
 		<< " hang time: " << hangTime << "s" << endl;
 }
 
+double getGravity(double altitude, const vector<double>& altitudes, const vector<double>& gravities)
+{
+    if (altitude <= altitudes.front())
+        return gravities.front();
+
+    for (size_t i = 0; i < altitudes.size() - 1; ++i)
+    {
+        if (altitude >= altitudes[i] && altitude <= altitudes[i + 1])
+        {
+            return linearInterpolation(
+                gravities[i],
+                gravities[i + 1],
+                altitudes[i],
+                altitudes[i + 1],
+                altitude
+            );
+        }
+    }
+
+    return gravities.back();
+}
+
+
+void gravity(double timeInterval, double initialSpeed, double initialAngle,
+    const vector<double>& altitudes, const vector<double>& gravities)
+{
+    double impactDistance = 0.0;
+    double impactAltitude = 0.0;
+    double hangTime = 0.0;
+
+    double angleRadians = degreesToRadians(initialAngle);
+    double dx = horizontalComponent(initialSpeed, angleRadians);
+    double dy = verticalComponent(initialSpeed, angleRadians);
+
+    while (impactAltitude >= 0.0)
+    {
+        double g = -getGravity(impactAltitude, altitudes, gravities);
+
+        impactDistance = distanceFormula(impactDistance, dx, timeInterval, 0.0);
+        impactAltitude = distanceFormula(impactAltitude, dy, timeInterval, g);
+        dy = kinematicsEquation(dy, g, timeInterval);
+
+        hangTime += timeInterval;
+    }
+
+    cout << "Distance: " << impactDistance << " m"
+        << "   Altitude: " << impactAltitude << " m"
+        << "   Hang Time: " << hangTime << " s" << endl;
+}
 // Main fuction
 int main()
 {
+    vector<double> altitudes = { 0.0, 1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, 7000.0, 8000.0, 9000.0, 10000.0, 15000.0, 20000.0, 25000.0, 30000.0, 40000.0, 50000.0, 60000.0, 70000.0, 80000.0 };
+	vector<double> gravities = { 9.807, 9.804, 9.801, 9.797, 9.794, 9.791, 9.788, 9.785, 9.782, 9.779, 9.776, 9.761, 9.745, 9.730, 9.715, 9.684, 9.654, 9.624, 9.594, 9.564 };
+
+
     cout << "Hello Dragon Snails!\n";
 	inertia(827.0, 75.0);
-	acceleration(-9.8);
-	groundImpact(0.01, -9.8, 827.0, 75.0);
+	acceleration(GRAVITY, 827.0, 75.0);
+	groundImpact(0.01, GRAVITY, 827.0, 75.0);
+    gravity(0.01, 827.0, 75.0, altitudes, gravities);
+
 
     return 0;
 }
